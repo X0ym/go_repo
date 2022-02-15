2a0e1d06c911942246436c4092d8dbda450b7e9b
Xieym <59227573+X0ym@users.noreply.github.com>
2022-02-15 21:39:00 +0800

    测试git rm --cached

--- src/server.go
+++ src/server.go
@@ -10,7 +10,7 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// 向DefaultServerMux添加处理器
+	//   向DefaultServerMux添加处理器
 	http.HandleFunc("/hello", sayHello)
 	// 指定端口，和处理器，nil表示使用包变量 DefaultServerMux 作为处理器
 	err := http.ListenAndServe(":9090", nil)
